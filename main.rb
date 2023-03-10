70f543ddbc9756b45a909d610d335e7929383099
tronross <john_ross@analogforever.ca>
2023-03-10 13:54:39 -0500

    Add Problem test code

--- main.rb
+++ main.rb
@@ -3,3 +3,15 @@ require './game'
 require './player'
 require './turn'
 require './problem'
+
+
+# playerOne = Player.new
+# puts playerOne.lives
+
+# playerOne.reduce_lives
+# puts playerOne.lives
+
+
+problemOne = Problem.new
+puts problemOne.num1
+puts problemOne.num2
