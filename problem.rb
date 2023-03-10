ef1232cc6c992d8d3cf0c546c710ecf572369851
tronross <john_ross@analogforever.ca>
2023-03-10 15:02:54 -0500

    Add @answer; refactor

--- problem.rb
+++ problem.rb
@@ -1,12 +1,12 @@
 # Problem generates the question using random numbers and calculates the correct answer (and assigns the question and answer to a hash).
 
 class Problem
-  attr_reader :problem, :num1, :num2
+  attr_reader :num1, :num2, :answer
 
   def initialize
     @num1 = (rand(1..20))
     @num2 = (rand(1..20))
-    
+    @answer = @num1 + @num2
   end
  
 end
