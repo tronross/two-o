477b8a750910034e8074220bf61f7cc026d68c68
tronross <john_ross@analogforever.ca>
2023-03-10 13:31:05 -0500

    Add comments

---
+++ game.rb
@@ -0,0 +1,9 @@
+# The Game class keeps track of the current player and score and orchestrates and initiates the turns; it decides the final winner and prints an appropriate message.
+
+class Game
+
+
+
+
+
+end
