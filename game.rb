2c49369e0b3efebdb4b7eabdfc75df757148ed62
tronross <john_ross@analogforever.ca>
2023-03-10 16:22:22 -0500

    Add basic turn logic; add initialize

--- game.rb
+++ game.rb
@@ -2,11 +2,28 @@
 
 class Game
 
+  def initialize
+    @player_one = Player.new
+    @player_two = Player.new
+    @current_player = 1
+  end
+ 
+  def turn 
+    problem = Problem.new
+    @current_player == 1 ? 2 : 1
+    puts "Player #{@current_player}: #{problem.q}"
+    answer = gets.chomp.to_i
+    if answer == problem.a
+      puts "Yass"
+    else
+      puts "Nooo"
+    end
+  end
 
-
-
+  def game_start
+    puts "Welcome Players!"
+    turn
+  end
 
 end
-
-
 
