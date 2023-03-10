b4265aaf6168602f1c3cd9c478040e7e0906599e
tronross <john_ross@analogforever.ca>
2023-03-10 15:09:39 -0500

    Add @question; refactor

--- problem.rb
+++ problem.rb
@@ -1,11 +1,12 @@
 # Problem generates the question using random numbers and calculates the correct answer (and assigns the question and answer to a hash).
 
 class Problem
-  attr_reader :num1, :num2, :answer
+  attr_reader :question, :answer
 
   def initialize
     @num1 = (rand(1..20))
     @num2 = (rand(1..20))
+    @question = "What does #{@num1} plus #{@num2} equal?"
     @answer = @num1 + @num2
   end
  
