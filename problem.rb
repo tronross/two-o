fba1cd72cf4620b34a36896e8af5f7cc8b44480e
tronross <john_ross@analogforever.ca>
2023-03-10 21:20:44 -0500

    Remove redundant code and :problem

--- problem.rb
+++ problem.rb
@@ -1,13 +1,12 @@
 # Problem generates the question using random numbers and calculates the correct answer (and assigns the question and answer to a hash).
 
 class Problem
-  attr_reader :question, :answer
+  attr_reader :q, :a
 
   def initialize
     @num1 = (rand(1..20))
     @num2 = (rand(1..20))
-    @question = "What does #{@num1} plus #{@num2} equal?"
-    @answer = @num1 + @num2
+    @q = "What does #{@num1} plus #{@num2} equal?"
+    @a = @num1 + @num2
   end
- 
 end
