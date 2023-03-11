0c0feda103a839a24b1e355e668ebe45f94cd77e
tronross <john_ross@analogforever.ca>
2023-03-10 21:18:56 -0500

    Remove test code; remove require Turn

--- main.rb
+++ main.rb
@@ -1,18 +1,7 @@
-# 
+# Brings all classes together; launches game.
 require './game'
 require './player'
-require './turn'
 require './problem'
 
-
-# playerOne = Player.new
-# puts playerOne.lives
-
-# playerOne.reduce_lives
-# puts playerOne.lives
-
-
-problemOne = Problem.new
-puts problemOne.num1
-puts problemOne.num2
-puts problemOne.answer
+game = Game.new
+game.game_start
